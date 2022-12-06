cbf91b9f3a262400861e0eece7bf31ad69ee9c83
Daniel Correa <fernandocorrea256@gmail.com>
2022-12-06 10:20:32 -0500

    basic

---
+++ main.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+import discord
+import os
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"Bot ready, logged as {client.user}")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("$hello"):
+        await message.channel.send("hello")
+
+client.run(os.environ.get("TOKEN"))
